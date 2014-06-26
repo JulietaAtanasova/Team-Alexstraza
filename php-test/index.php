e4a1519933acba42b65643a48c6e5500aa6c2181
coasterka <tsvetanovah@gmail.com>
2014-06-26 23:12:19 +0300

    uploaded working php pages

---
+++ php-test/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>SoftWars Test</title>
+		<link type="text/css"
+		rel="stylesheet"
+		href="test.css">
+	</head>
+	<body>
+		<?php
+
+		include("header.php");
+
+		?>
+
+		</br>
+
+		<div id="content">
+			<h1>PHP is badass</h1>
+		</div>
+
+		</br>
+	</body>
+</html>

7f8f763da01a7e4dbf8c8ac779bd2d62480f54eb
coasterka <tsvetanovah@gmail.com>
2014-07-02 22:09:56 +0300

    uploaded new and fixed files

---
+++ header.php
@@ -0,0 +1,69 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link type="text/css"
+        rel="stylesheet"
+        href="styles/university.css">
+	<link rel="shortcut icon" type="image/x-icon" href="images/logo-icon.ico"/>
+</head>
+<body>
+    <div id="head-container">
+        <nav class="nav-container">
+            <a href="index.php" >
+                <img id="logo" src="images/logo-full.png" alt="logo" /></a>
+            <ul class="menu-item">
+                <li>
+                    <a href="news.php" title="news" >Новини</a>
+                </li>
+                <li>
+                    <a href="#">За университета ▾</a>
+                    <ul class="sub-nav-list">
+                        <li>
+                            <a href="about.php" title="aboutUs" >За нас</a>
+                        </li>
+                        <li>
+                            <a href="university-partners.php" title="partners" >Университети-партньори</a>
+                        </li>
+                        <li>
+                            <a href="trainers.php" title="trainers" >Орденът на джедаите</a>
+                        </li>
+                        <li>
+                            <a href="hospice.php" title="dorms" >Общежития</a>
+                        </li>
+                        <li>
+                            <a href="registration.php" title="apply" >Кандидатстване</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Обучения ▾</a>
+                    <ul class="sub-nav-list">
+                        <li>
+                            <a class="sub-menu" href="#" title="courses" >Курсове</a>
+                        </li>
+                        <li>
+                            <a href="#" title="java basics may 2014" >Java Basics May 2014</a>
+                        </li>
+                        <li>
+                            <a href="#" title="c# basics march 2014" >C# Basics march 2014</a>
+                        </li>
+                        <li>
+                            <a href="#" title="web fundamentals" >Web Fundamentals (HTML + CSS) May 2014</a>
+                        </li>
+                        <li>
+                            <a href="#" title="teamwork" >Teamwork and personal skills May 2014</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#" title="news" >Работа</a>
+                </li>
+                <li>
+                    <a href="contacts.php" title="news" >Контакти</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</body>
+</html>

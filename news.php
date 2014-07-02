23b6b5deeeb464a4d9370445d005fe8e1517bf94
coasterka <tsvetanovah@gmail.com>
2014-07-02 13:23:24 +0300

    uploaded news.php

---
+++ news.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>	
+	<head>
+		<meta charset="UTF-8" />
+		<link type="text/css" rel="stylesheet" href="styles/styles.css">
+	</head>
+	<body>
+		<div id="news">
+			
+			<div id="wrapper">
+				<?php
+		
+					include("header.php");
+		
+				?>
+				<h1>
+					Новини
+				</h1>
+				<div id="content">
+					<section>
+				        <article>
+				            <header>Мастер Наков направи собствен университет</header>
+				            <div class="date">21/08/2013</div>
+				            <div class="author">Публикувано от: Emo-Windu</div>
+				            <img src="images/master-nakov02.jpg" width="150" height="150"/>
+				            <div class="text">Наков: Ако не искаш да учиш, никой не може да ти помогне. Ако си решен да учиш, никой не може да те спре.</div>
+				            <div class="tags"><span>Тагове:</span> softwars-university green goblin creater little guy sword</div>
+				        </article>
+				        <article>
+				            <header>Нов учител в SoftWars - Оби-Уан-Евлоги</header>
+				            <div class="date">13/06/2014</div>
+				            <div class="author">Публикувано от: Han-Joro</div>
+				            <img src="images/obi-wan-evlogi.jpg" width="150" height="150"/>
+				            <div class="text">Оби-Уан: Почувстах голяма безредица в Силата, сякаш милиони гласове плачеха от ужас и изведнъж замлъкнаха.</div>
+				            <div class="tags"><span>Тагове:</span> software-university obi wan evlogi blue sword</div>
+				        </article>
+				        <article>
+				            <header>Чисто нови коли за стипендиантите</header>
+				            <div class="date">02/06/2014</div>
+				            <div class="author">Публикувано от: Hriss-Offi</div>
+				            <img src="images/AC-BMW.jpg" width="150" height="150" alt="softuni conf" />
+				            <div class="text">BMW M6 Grand Coupe. Powered by AC-Design.</div>
+				            <div class="tags"><span>Тагове:</span> softwars-university BMW tarataiki</div>
+				        </article>
+				        <article>
+				            <header>Реал Мадрид предизвиква отбора на SoftWars</header>
+				            <div class="date">02/06/2014</div>
+				            <div class="author">Публикувано от: Emo-WIndu</div>
+				            <img src="images/real-madrid.png" width="150" height="150" />
+				            <div class="text">Отговори на анкетата за безполезността на висшата лига.</div>
+				            <div class="tags"><span>Тагове:</span> softwars-university football real madrid major league</div>
+				        </article>
+				        <article>
+				            <header>Порноактриса иска да учи в SofWars University</header>
+				            <div class="date">02/06/2014</div>
+				            <div class="author">Публикуван от: Julieta-Amidala</div>
+				            <img src="images/briana-banks.jpg" width="150" height="150" />
+				            <div class="text">Бриана Банкс: Много се вълнувам за новите си колеги от SoftWars Uni. I'll blow your mind.</div>
+				            <div class="tags"><span>Тагове:</span> softwars-university Briana Banks porno</div>
+				        </article>
+				        <article>
+				            <header>July morning</header>
+				            <div class="date">02/07/2014</div>
+				            <div class="author">Публикувано от: Julieta-Amidala</div>
+				            <img src="images/july-morning.jpg" width="150" height="150" />
+				            <div class="text">Студенти от SoftWars Uni посрещат заедно Джулай морнинг на Нептун.</div>
+				            <div class="tags"><span>Тагове:</span> softwars-university july morning</div>
+				        </article>
+				        <footer>
+				            
+				        </footer>
+				    </section>
+				</div>
+				
+			<?php
+	
+	        	include("footer.php");
+	
+	        ?>
+			</div>
+			
+		</div>
+	</body>
+</html>
